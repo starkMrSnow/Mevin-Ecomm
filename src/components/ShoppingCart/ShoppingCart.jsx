942154d58a0f5e6c5472e6c6ab942b1ae8a41b4d
starkMrSnow <175dollarsnow@gmail.com>
2024-11-11 10:26:46 +0300

    complete set up

---
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ShoppingCart.css'
+
+export default function ShoppingCart() {
+  return (
+    <div>
+       <div>
+        navbar
+
+       </div>
+    </div>                 
+  )             
+}
