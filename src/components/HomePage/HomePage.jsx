07523eaf1d3e5baedc691bc20dc2d304ccf19b15
starkMrSnow <175dollarsnow@gmail.com>
2025-01-22 12:02:46 +0300

    removed the blue margin

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,8 +6,7 @@ import { GoSearch } from "react-icons/go";
 import { VscAccount } from "react-icons/vsc";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { IoCartOutline } from "react-icons/io5";
-import vixen from '../../assets/vixen.png'
-
+import vixen from '../../assets/vixen.png';
 
 export default function HomePage() {
   const [query, setQuery] = useState('');
@@ -23,13 +22,13 @@ export default function HomePage() {
   return (
     <>
       <div>
-        <div className='top'></div>
-        <div className='bottom'>
-          <FaBars className='hamburger' />
+        <div className="top"></div>
+        <div className="bottom">
+          <FaBars className="hamburger" />
           <img src={logo} alt="Logo" className="logo" />
 
           <div className="search-bar">
-            <div className='search-input'>
+            <div className="search-input">
               <input
                 type="text"
                 value={query}
@@ -39,41 +38,39 @@ export default function HomePage() {
             </div>
 
             <button className="search-button" onClick={handleSearch}>
-              <GoSearch className='searchicon' />
+              <GoSearch className="searchicon" />
             </button>
           </div>
-          <div className='row'>
-          <VscAccount className='account'/>
-          <h6>ACCOUNT</h6>
-          <RiArrowDropDownLine className='dropdown'/>
-          <IoCartOutline className='cart'/>
-          <h6>Cart</h6>
-          <RiArrowDropDownLine className='dropdown'/>
+          <div className="row">
+            <VscAccount className="account" />
+            <h6>ACCOUNT</h6>
+            <RiArrowDropDownLine className="dropdown" />
+            <IoCartOutline className="cart" />
+            <h6>Cart</h6>
+            <RiArrowDropDownLine className="dropdown" />
           </div>
         </div>
       </div>
 
-      <div className='rectangle'>
-    
+      <div className="rectangle">
         <div className="grid-container">
-      <div className="grid-item left-grid">
+          <div className="grid-item left-grid">
             <h5>Today's pick</h5>
-            <h3 className='w'>WOMEN'S WEAR</h3>
-            <h6 className='offer'>GET 70% OFF</h6>
-            <div className='btn'>
-                <button className='btnOne'>ORDER NOW</button>
-                <button className='btnTwo'>VIEW MORE</button>
+            <h3 className="w">WOMEN'S WEAR</h3>
+            <h6 className="offer">GET 70% OFF</h6>
+            <div className="btn">
+              <button className="btnOne">ORDER NOW</button>
+              <button className="btnTwo">VIEW MORE</button>
             </div>
-      </div>
-      <div className="grid-item right-grid">
-      <img src={vixen} alt="vixen" className='vixen' />
-      </div>
-    </div>
-    
+          </div>
+          <div className="grid-item right-grid">
+            <img src={vixen} alt="vixen" className="vixen" />
+          </div>
+        </div>
       </div>
 
-      <div>
-        footer
+      <div className="content">
+        <p>stanley</p>
       </div>
     </>
   );
