0559a343be23728c03ac929b86c9e8beadd8aab6
Mash <marshmisherk@gmail.com>
2025-01-27 15:07:36 +0300

    Order confirmation page creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+//import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart.jsx';
 import ProductDetail from './components/ProductDetail/ProductDetail.jsx';
 import HomePage from './components/HomePage/HomePage.jsx';
+import OrderConfirmation from './components/OrderConfirmation/OrderConfirmation.jsx';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/ShoppingCart" element={<ShoppingCart />} />
           <Route path="/ProductDetail" element={<ProductDetail />} />
           <Route path="/HomePage" element={<HomePage />} />
+          <Route path= "/OrderConfirmation" element={<OrderConfirmation />} />
         </Routes>
       </div>
     </Router>
