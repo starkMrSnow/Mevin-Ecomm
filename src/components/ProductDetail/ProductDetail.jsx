096ddfd52802d09075815b211a8ec5c0f77af129
fatihaosman17@gmail.com <fatihaosman17@gmail.com>
2025-01-29 01:37:37 +0300

    product-detail-header

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,9 +1,135 @@
 import React from 'react'
-
+import './ProductDetail.css'
 export default function productdetail() {
   return (
     <div>
-      
+        <div className="product-deatil-headers"> 
+            <p>FEATURED</p>
+            <p className='select'>LATEST</p>
+            <p className='select'>TOP PICKS</p>
+        </div>
+        <div className='productdetail-Container'>
+          <div className="productdetail-smallContainer">
+                <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+          <div className="productdetail-smallContainer">
+          <div className="image"><img src="" alt="" /></div>
+                <div className="desc">
+                    <p className="product-name"></p>
+                    <div className="price-ratings">
+                        <p className="price"></p>
+                        <img src="" alt="" className="ratings" />
+                    </div>
+                </div>
+          </div>
+        </div>
     </div>
   )
 }
